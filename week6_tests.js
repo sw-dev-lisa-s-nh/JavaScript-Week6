fd2e8382333144e0821e75b44046940bc4f5a899
Lisa Smith <las619@comcast.net>
2021-11-14 17:44:45 -0500

    First Try at Unit Tests

--- week6_tests.js
+++ week6_tests.js
@@ -2,20 +2,64 @@
 var expect = chai.expect;
 
 // takes a "name", and a function that sets up the test
-describe('MyWeek6Functions', function() {
-    describe('#doSomething', function() {
-        it('should concatenate the two parameters', function() {
+describe('LisasWeek6Functions', function() {
+    describe('#populateCards', function() {
+
+        it('should create a 52 card array', function() {
             // actual call to function
-            var x = doSomething("Hello", 5);
-            expect(x).to.equal('Hello5');
+            var deck = new Deck();
+            var cards = deck.populateCards();
+            expect(cards).length(52);
         });
 
-        it('should throw an error if first parameter is not a string', function() {
-            // wrapped in an expect, because we expect it to throw an error
-            expect(function() {
-                doSomething(5,5);
-            }).to.throw(Error);
+        it('should create 4 of each card value', function() {
+            // create an array with a place for each card value.
+            var cardCount = [0,0,0,0,0,0,0,0,0,0,0,0,0];
+            var deck = new Deck();
+            var cards = deck.populateCards();
+            // count each value, and increment the array position
+            for (let card of cards) {
+                cardCount[card.value-2]++;
+            };
+            var number = cardCount.reduce(function(previous,current) {
+                return previous + current;
+            }, 0);
+            number = number/13;
+            expect(number).to.equals(4);
         });
+
+        it('should create 13 of each suit value', function() {
+            // create an array with a place for each card value.
+            var heartCount = 0;
+            var spadeCount = 0;
+            var clubCount = 0;
+            var diamondCount = 0;
+            var deck = new Deck();
+            var cards = deck.populateCards();
+            // count each value, and increment the array position
+            for (let card of cards) {
+                if (card.suit == "Hearts") {
+                    heartCount++;
+                } else if (card.suit == "Spades") {
+                    spadeCount++;
+                } else if (card.suit == "Diamonds") {
+                    diamondCount++;
+                } else if (card.suit == "Clubs") {
+                    clubCount++;
+                }
+            };
+            expect(heartCount).to.equals(13);
+            expect(spadeCount).to.equals(13);
+            expect(clubCount).to.equals(13);
+            expect(diamondCount).to.equals(13);
+        });
+
+        // it('should throw an error if first parameter is not a string', function() {
+        //     // wrapped in an expect, because we expect it to throw an error
+        //     expect(function() {
+        //         populateCards();
+        //     }).to.throw(Error);
+        // });
     });
 
 });
